9d74035a90e66f17a6629eed95d99d9ea61f5e7f
vizit1996 <vizitBhardwaj1996@gmail.com>
2023-02-14 13:09:25 +0530

    rest api connection

--- src/App.js
+++ src/App.js
@@ -1,16 +1,39 @@
-import Backdrops from "./components/Backdrops";
-import Modal from "./components/Modal";
-import Todo from "./components/Todo";
+import React , {useState} from 'react';
 
+import MoviesList from './components/MoviesList';
+import './App.css';
 
 function App() {
+const [movies , setMovies ] = useState([]);
 
-  return <div>
-    <h1>MY TODOS</h1>
-    <Todo text = 'react'  />
-    <Todo text = 'nextjs' />
-    <Todo text = 'nodejs' />
-  </div>
+  function fetchMoviesHandler(){
+    fetch('https://swapi.dev/api/films/').then(response => {
+      return response.json();
+    }).then(data => {
+      const transformedData = data.results.map(moviesData => {
+        return{
+          id : moviesData.episode_id,
+          title : moviesData.title,
+          openingText : moviesData.opening_crawl,
+          releaseDate : moviesData.release_date
+        }
+      }
+
+      )
+      setMovies(transformedData);
+    });
+  }
+
+  return (
+    <React.Fragment>
+      <section>
+        <button onClick={fetchMoviesHandler}>Fetch Movies</button>
+      </section>
+      <section>
+        <MoviesList movies={movies} />
+      </section>
+    </React.Fragment>
+  );
 }
 
 export default App;
--- src/components/Backdrops.js
+++
@@ -1,8 +0,0 @@
-function Backdrops(props){
-    return(
-        <div className='backdrop' onClick={props.onCancel}/>
-    );
-
-}
-
-export default Backdrops
